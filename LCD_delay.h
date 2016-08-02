129dca6c88b4a98f5dcc84de5a70821c897e3291
Anirban Raha <rahaanirban91@gmail.com>
2016-08-02 12:27:03 +0530

    delete LCD_delay.h as it is not required
    Add simple delay function with loop

--- LCD_delay.h
+++
@@ -1,6 +0,0 @@
-#include "lpc_types.h"
-void TIMER0_IRQHandler (void);
-uint32_t getPClock (uint32_t);
-uint32_t converUSecToVal (uint32_t, uint32_t);
-void Timer0_init(uint32_t);
-void delay_ms(unsigned int);
